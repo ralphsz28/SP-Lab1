c0cb7cda33d9da124a3c610548180b2812ea6a1d
ralphsz28 <ralph.szakacs01@e-uvt.ro>
2022-12-21 10:30:55 +0200

    tema4

--- src/main.java
+++ src/main.java
@@ -3,30 +3,32 @@ public class main {
 
 	public static void main(String[] args) {
 		
-		Book noapteBuna = new Book("Noapte buna, copii!");
-		Author rpGheo = new Author("Radu Pavel Gheo");
-		noapteBuna.addAuthor(rpGheo);
-		
-		Section cap1 = new Section("Capitolul 1");
-		Paragraph p1 = new Paragraph("Paragraph 1");
-		cap1.add(p1);
-		Paragraph p2 = new Paragraph("Paragraph 2");
-		cap1.add(p2);
-		Paragraph p3 = new Paragraph("Paragraph 3");
-		cap1.add(p3);
-		Paragraph p4 = new Paragraph("Paragraph 4");
-		cap1.add(p4);
-		System.out.println("Printing without Alignment");
-		System.out.println();
-		cap1.print();
-		p1.setAlignStrategy(new AlignCenter());
-		p2.setAlignStrategy(new AlignRight());
-		p3.setAlignStrategy(new AlignLeft());
+		long startTime = System.currentTimeMillis();
+		Image img1 = new Image("Pamela Anderson");
+		Image img2 = new Image("Kim Kardashian");
+		Image img3 = new Image("Kirby Griffin");
+		Section playboyS1 = new Section("Front Cover");
+		playboyS1.add(img1);
+		Section playboyS2 = new Section("Summer Girls");
+		playboyS2.add(img2);
+		playboyS2.add(img3);
+		Book playboy = new Book("Playboy");
 
-		System.out.println();
-		System.out.println("Printing with Alignment");
-		System.out.println();
-		cap1.print();
+		playboy.addContent(playboyS1);
+		playboy.addContent(playboyS2);
+		long endTime = System.currentTimeMillis();
+		System.out.println("Creation of the content took " + (endTime -
+		startTime) + " milliseconds");
+		startTime = System.currentTimeMillis();
+		playboyS1.print();
+		endTime = System.currentTimeMillis();
+		System.out.println("Printing of the section 1 took " + (endTime -
+		startTime) + " milliseconds");
+		startTime = System.currentTimeMillis();
+		playboyS1.print();
+		endTime = System.currentTimeMillis();
+		System.out.println("Printing again the section 1 took " + (endTime -
+		startTime) + " milliseconds");
 		
 	}
 
